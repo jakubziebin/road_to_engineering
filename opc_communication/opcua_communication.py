f68771ef06144327f720bf8a4947cd3583ca4120
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-03-09 21:00:27 +0100

    Change master of opcua communnication for tia portal

--- opc_communication/opcua_communication.py
+++ opc_communication/opcua_communication.py
@@ -1,39 +1,18 @@
-from opcua import Server
+from opcua import Client
 
-from time import sleep
-"""
-    This project use to find cheaper way to heat a water. Choose is between heaters which use gas and electricity.
-    It is very comfortable situasion with electricity, cause in Poland we have available site, which shows everyday
-    electricity prices for every hour. Worse situation is with gas. Our government organisation give us information 
-    once by 6 months about it. It definitely not include dynamic of changes gas prices on global markets. 
-    I decided to fix it with machine learning model, which would be forecast prices of gas in Poland.
-"""
+def read_value(node_id):
+    client_node = client.get_node(node_id)
+    client_node_value = client_node.get_value()
+    print(client_node_value)
 
-server = Server()
-server.set_endpoint("opc.tcp://localhost:4841")
-namespace = server.register_namespace("Prices_of_Energy_and_Source_Choose")
 
-objects = server.get_objects_node()
-prices_and_choose = objects.add_object(namespace, "PoE")
+if __name__ == "__main__":
+    # Connect to opc ua server implemented in Tia Portal
+    client = Client('opc.tcp://192.168.1.10:4840')
+    client.connect()
 
-price_of_gas = prices_and_choose.add_variable(namespace, "price_of_gas", 11.1)
-price_of_electricity = prices_and_choose.add_variable(namespace, "price_of_electricity", 11.1)
-use_electricity = prices_and_choose.add_variable(namespace, "price_of_electricity", False)
-use_gas = prices_and_choose.add_variable(namespace, "price_of_electricity", False)
+    variables_in_plc = ("PriceOfElectricity", "PriceOfGas", "UseGas", "UseElectricity")
 
-all_variables = ("price_of_gas", "price_of_electricity", "use_electricity", "use_gas")
-for variable in all_variables:
-    eval(f"{variable}.set_writable()")
-
-server.start()
-print("Server start")
-while True:
-    print("Server is still here")
-    use_gas.set_value(True)
-    use_electricity.set_value(True)
-    price_of_gas.set_value(1000.1)
-    price_of_electricity.set_value(1000.1)
-    print(price_of_electricity.get_value())
-    sleep(20)
+    read_value('ns=3;s="OpcuaPythonTransfer"."PriceOfElectricity"')
 
 
