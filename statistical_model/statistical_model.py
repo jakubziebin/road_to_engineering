922990e964035fb4b2ad346799f262659ee05316
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-04-10 11:17:28 +0200

    Refactor structure of project, first progress in statistical model

--- statistical_model/statistical_model.py
+++ statistical_model/statistical_model.py
@@ -1,11 +1,13 @@
 import os    
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+import numpy as np
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sktime import *
+from sktime.forecasting.model_selection import temporal_train_test_split
+from sktime.forecasting.theta import ThetaForecaster
 
 # Read values from csv file
 with open('ceny_gazu.csv') as f:
@@ -20,6 +22,18 @@ with open('ceny_gazu.csv') as f:
 # create data frame object
 prices = pd.DataFrame({'price': prices})
 
-# represent our time serie
-plt.plot(prices.index, prices['price'])
+
+# represent our time series
+#plt.plot(prices.index, prices['price'])
+#plt.show()
+
+gas_train, gas_test = temporal_train_test_split(prices)
+
+fh = np.arange(1, len(gas_test) + 1)  # forecasting horizon
+forecaster = ThetaForecaster(sp=12)  # monthly seasonal periodicity
+forecaster.fit(gas_train)
+
+y_pred = forecaster.predict(fh)
+
+plt.plot(gas_test)
 plt.show()
