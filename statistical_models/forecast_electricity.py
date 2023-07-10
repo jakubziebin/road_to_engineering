9117d245b1bd182d6c27aac880b02e202270dcbc
Jakub Ziębiński <ziebinskijakub@gmail.com>
2023-07-10 20:51:18 +0200

    Add SARIMA model to forecast electricity prices

--- statistical_models/forecast_electricity.py
+++ statistical_models/forecast_electricity.py
@@ -2,27 +2,68 @@ from __future__ import annotations
 import os    
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import adfuller, kpss
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
 
-
-with open('ceny_energii_elektrycznej.csv') as f:
+def read_value_from_csv(file_name: str) -> list[float]:
+    prices = []
+    with open(file_name) as f:
         reader = csv.reader(f)
         header = next(reader)
     
-        prices = []
         for row in reader:
             row = row[0].split(";")
-            if row[1] == '12':
-                if "1ďż˝" in row[2]:
-                    price = row[2].split("1ďż˝")
-                    price = f"{price[0]}{price[1]}"
-                else:
-                    price = row[2]
-                prices.append(float(price))
+            try:
+                price = float(row[2])
+            except ValueError:
+                 print("I did what i could sorry :(") 
+                 continue
+            prices.append(price)
+    return prices
 
 
-prices_df = pd.DataFrame({'price': prices})
-print(prices_df)
+def analise_time_serie(prices: pd.DataFrame) -> None:
+    """adfuller test and kpss showed that time serie is stationary, so there is no reason difference it"""
+    result_decompose = seasonal_decompose(prices_df, period=len(prices_df)//2)
+    seasonal_part = result_decompose.seasonal
 
+
+    adfuller_result = adfuller(prices_df)
+    print(f'p-value for non-differenced serie: {adfuller_result[1]}')
+
+    kpss_result = kpss(prices_df)
+    print(f"kpss_result for non-differenced serie: {kpss_result}")
+
+    plot_acf(prices_df)
+    plt.show()
+
+    plot_pacf(prices_df)  # parameter p = 2
+    plt.show()
+
+    plot_acf(seasonal_part)
+    plt.show()
+
+    plot_pacf(seasonal_part)  # parameter P = 2
+    plt.show()
+    
+
+def create_model(prices: pd.DataFrame, horizont: int) -> pd.DataFrame:
+    model = SARIMAX(prices, order=(2, 0, 2), seasonal_order=(2, 0, 2, 24))
+    model_fit = model.fit()
+    predictions = model_fit.predict(start=0, end=len(prices)+horizont)
+    return predictions
+
+
+if __name__ == "__main__":
+    prices = read_value_from_csv('ceny_energii_elektrycznej.csv')
+    prices_df = pd.DataFrame({'price': prices})
+
+    test = create_model(prices_df, 1000)
+    test.plot()
+    plt.plot(prices)
+    plt.show()
